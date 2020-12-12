8a734e94c374e72b4aead5e3cdec3717b9aff373
lidonglang <944976717@qq.com>
2020-12-12 21:46:06 +0800

    部分framework的注释添加

--- blog-core/src/main/java/com/zyd/blog/framework/config/AsyncConfig.java
+++ blog-core/src/main/java/com/zyd/blog/framework/config/AsyncConfig.java
@@ -30,54 +30,101 @@ import java.util.concurrent.Future;
 @EnableAsync
 public class AsyncConfig implements AsyncConfigurer {
 
+    /**
+     * 覆写函数，返回自定义线程池
+     * @return 自定义线程池
+     */
     @Override
     @Bean
     public Executor getAsyncExecutor() {
         return new ContextAwarePoolExecutor();
     }
 
+    /**
+     *捕捉处理线程抛出的异常
+     * @return 自定义异常
+     */
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
         return new SpringAsyncExceptionHandler();
     }
 
+    /**
+     *自定义一个异步方法抛出的未捕获异常
+     */
     class SpringAsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+        //用于处理异步方法抛出的未捕获异常
         @Override
         public void handleUncaughtException(Throwable throwable, Method method, Object... objects) {
+            //日志记录错误
             log.error("异步线程发生异常！Method [{}]，Error Message [{}]", method.getName(), throwable.getMessage());
         }
     }
 
+    /**
+     * 自定义一个线程池
+     */
     public class ContextAwarePoolExecutor extends ThreadPoolTaskExecutor {
+
+
+        /**
+         * Callable类似与Runnable，不过Callable可以返回线程运行的结果
+         * 提交值返回任务以执行，并返回结果
+         * @param task
+         * @param <T>
+         * @return 线程运行的结果
+         */
         @Override
         public <T> Future<T> submit(Callable<T> task) {
             return super.submit(new ContextAwareCallable(task, RequestContextHolder.currentRequestAttributes()));
         }
 
+        /**
+         *  提交一个Callable任务，将来完成时将会返回结果
+         * @param task
+         * @param <T>
+         * @return 将来完成后返回完成的结果
+         */
         @Override
         public <T> ListenableFuture<T> submitListenable(Callable<T> task) {
             return super.submitListenable(new ContextAwareCallable(task, RequestContextHolder.currentRequestAttributes()));
         }
     }
 
+    /**
+     * 自定义Callable中的方法
+     * @param <T>
+     */
     public class ContextAwareCallable<T> implements Callable<T> {
         private Callable<T> task;
+
+        //包含Request中的属性
         private RequestAttributes context;
 
+        //构造函数
         public ContextAwareCallable(Callable<T> task, RequestAttributes context) {
             this.task = task;
             this.context = context;
         }
 
+        /**
+         * 自定义call方法
+         * @return
+         * @throws Exception
+         */
         @Override
         public T call() throws Exception {
+            //如果context为空，则将RequestContextHolder中的RequestAttributes设置为context
             if (context != null) {
                 RequestContextHolder.setRequestAttributes(context);
             }
 
             try {
+                //使用父类Callable中的call方法
                 return task.call();
             } finally {
+                //最后重置RequestContextHolder中的RequestAttributes
                 RequestContextHolder.resetRequestAttributes();
             }
         }
--- blog-core/src/main/java/com/zyd/blog/framework/holder/RequestHolder.java
+++ blog-core/src/main/java/com/zyd/blog/framework/holder/RequestHolder.java
@@ -12,11 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * @author yadong.zhang (yadong.zhang0415(a)gmail.com)
- * @version 1.0
- * @website https://www.zhyd.me
- * @date 2018/4/16 16:26
- * @since 1.0
+ * 用于保存request，response，session数据
+ * 用户登陆时会将信息存在此类
+ * 将用户信息存储在holder中，需要时可以直接取
  */
 @Slf4j
 public class RequestHolder {
@@ -24,14 +22,22 @@ public class RequestHolder {
     /**
      * 获取request
      *
-     * @return HttpServletRequest
+     * @return HttpServletRequest 用户的HttpServletRequest
      */
     public static HttpServletRequest getRequest() {
+
+        //日志记录获取request的线程信息
         log.debug("getRequest -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
+
+        //从 RequestContextHolder中获得RequestAttributes信息
         ServletRequestAttributes servletRequestAttributes = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes());
+
+        //如果获得的RequestAttributes信息为空，则直接返回空，避免报错
         if (null == servletRequestAttributes) {
             return null;
         }
+
+        //如果不为空，则调用RequestAttributes的getRequest方法，获得request
         return servletRequestAttributes.getRequest();
     }
 
@@ -41,11 +47,19 @@ public class RequestHolder {
      * @return HttpServletRequest
      */
     public static HttpServletResponse getResponse() {
+
+        //日志记录获取response的线程信息
         log.debug("getResponse -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
+
+        //从 RequestContextHolder中获得RequestAttributes信息
         ServletRequestAttributes servletRequestAttributes = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes());
+
+        //如果获得的RequestAttributes信息为空，则直接返回空，避免报错
         if (null == servletRequestAttributes) {
             return null;
         }
+
+        //如果不为空，则调用RequestAttributes的getRequest方法，获得response
         return servletRequestAttributes.getResponse();
     }
 
@@ -55,11 +69,18 @@ public class RequestHolder {
      * @return HttpSession
      */
     public static HttpSession getSession() {
+
+        //日志记录获取session的线程信息
         log.debug("getSession -- Thread id :{}, name : {}", Thread.currentThread().getId(), Thread.currentThread().getName());
+
         HttpServletRequest request = null;
+
+        //使用上面的getRequest方法获取HttpServletRequest，并判断是否为空
         if (null == (request = getRequest())) {
             return null;
         }
+
+        //如果不为空，则从HttpServletRequest中取出session
         return request.getSession();
     }
 
